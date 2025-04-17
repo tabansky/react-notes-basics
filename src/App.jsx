06c5b9e4569fb234ee0607234a55ffc492d2a369
tabansky <talex.alexandr@gmail.com>
2025-04-17 11:21:28 +0200

    Lists and .map()

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import './App.css'
 import Note from './components/Note'
 
 function App() {
+  const notes = [
+    { id: 1, title: "Name of the note", text: "Content of the note" },
+    { id: 2, title: "Name of the note 2", text: "Content of the note 2" },
+    { id: 3, title: "New note", text: "Need to use useEffect!" },
+    { id: 4, title: "Map", text: "Map works" },
+  ]
+
   return (
     <div className='app'>
       <h1>React Notes Basics</h1>
 
-      <Note title = "Name of the note" text = "Content of the note"></Note>
-      <Note title = "Name of the note 2" text = "Content of the note 2"></Note>
-      <Note title="New note" text="Need to use useEffect!" />
+      {notes.map((note) => (
+        <Note key={note.id} title={note.title} text={note.text} />
+      ))}
     </div>
   )
 }
