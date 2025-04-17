ebe69af9a0aa635e392b1e66bb3a79e95aa967e0
tabansky <talex.alexandr@gmail.com>
2025-04-17 10:53:46 +0200

    Components + JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      Hello World!
-    </>
+    <div className='app'>
+      <h1>React Notes Basics</h1>
+
+      <div className='note'>
+        <h2>Name of the note</h2>
+        <p>Content of the note</p>
+      </div>
+
+      <div className='note'>
+        <h2>Name of the note 2</h2>
+        <p>Content of the note 2</p>
+      </div>
+    </div>
   )
 }
 
